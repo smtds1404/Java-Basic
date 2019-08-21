81dd8f7dbcac79fe93e68b834c016235ba609fa4
AE12849 <AE12849@USVMABS143.axafinancialsvc.axa-financial.INTRAXA>
2019-08-21 10:19:06 -0400

    Action Class

--- Sel_Java/src/JavascriptAlert.java
+++ Sel_Java/src/JavascriptAlert.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 
 public class JavascriptAlert {
 
